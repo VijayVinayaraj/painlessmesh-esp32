70a7d5e844717676fdf98c2dc536de1d35fc868f
VijayVinayaraj <vijayvinayaraj@gmail.com>
2023-07-29 22:50:34 +0530

    added relay control

--- painlessmesh-childnode/src/main.cpp
+++ painlessmesh-childnode/src/main.cpp
@@ -21,6 +21,9 @@
 #define ROOTNODE 1375526329
 
 
+#define RELAYPIN 26
+uint8_t state = 0;
+
 // Prototypes
 void receivedCallback(const uint32_t &from, const String &msg);
 
@@ -35,12 +38,12 @@ WiFiClient wifiClient;
 
 void sendMsgToBroker();
 Task taskSendMessage( TASK_SECOND * 3 , TASK_FOREVER, &sendMsgToBroker );
-
+float getPower(uint8_t state);
 
 void setup()
 {
   Serial.begin(115200);
-
+  pinMode(RELAYPIN,OUTPUT);
   mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION); // set before init() so that you can see startup messages
 
   // Channel set to 6. Make sure to use the same channel for your mesh and for you other
@@ -68,7 +71,7 @@ void loop()
 
 void sendMsgToBroker(){
   String msg = "";
-  msg += 78;
+  msg += getPower(state);
   // msg += mesh.getNodeId();
 
   // Serial.printf("test");
@@ -79,16 +82,53 @@ void sendMsgToBroker(){
 
 
 
-void toggleRelay(String state){
-  if (state == "1"){
+void toggleRelay(String relaystate){
+  if (relaystate == "1"){
     Serial.println("turned on");
-  }else if (state == "0"){
+    digitalWrite(RELAYPIN,HIGH);
+    state=1;
+  }else if (relaystate == "0"){
 Serial.println("turned off");
+    digitalWrite(RELAYPIN,LOW);
+
+    state=0;
   }
   else{
     Serial.println("error in toggle");
   }
  }
+
+
+// Function to generate a random float number within a given range
+float randomFloat(float minVal, float maxVal) {
+    // Ensure that minVal is smaller than maxVal
+    if (minVal > maxVal) {
+        float temp = minVal;
+        minVal = maxVal;
+        maxVal = temp;
+    }
+
+    // Generate a random integer between 0 and RAND_MAX
+    int randomInt = random(0, RAND_MAX);
+
+    // Convert the random integer to a float between 0 and 1
+    float randomFloat = static_cast<float>(randomInt) / static_cast<float>(RAND_MAX);
+
+    // Scale and shift the random float to the desired range
+    float result = randomFloat * (maxVal - minVal) + minVal;
+
+    return result;
+}
+
+
+
+
+float getPower(uint8_t state){
+  float watt = randomFloat(4,6);
+  return watt * state;
+}
+
+
 
 void receivedCallback(const uint32_t &from, const String &msg)
 {
